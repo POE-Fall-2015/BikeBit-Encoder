cb63be26b3f2c493124f64dc1315a4b98db565d8
Dennis Chen <dennis.chen@students.olin.edu>
2015-12-10 00:31:52 -0500

    finalized photon code with interrupts

--- oldArduinoCode/EncoderTesting/EncoderTesting.ino
+++ oldArduinoCode/EncoderTesting/EncoderTesting.ino
@@ -1,36 +1,9 @@
-int sensorPin = A0;    // select the input pin for IR sensor
-int sensorValue = 0;  // variable to store the value coming from sensor
-boolean oldStateTape = false;
-boolean currentStateTape = false;
-int counter = 0;
-long lastUpdatedTime = millis();
-boolean sentData = false;
+int sensorPin = A0;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  //Serial.print("count: ");
-  //Serial.println(counter);
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-   Serial.println(sensorValue);
-  if (sensorValue > 400){
-    currentStateTape = true;
-  }else{
-    currentStateTape = false;
-  }
-  if (oldStateTape != currentStateTape && currentStateTape){
-    counter+=1; 
-    lastUpdatedTime = millis();
-    sentData = false;
-  } 
-  if (millis() - lastUpdatedTime > 5000 && !sentData){
-    Serial.println(counter); // printing count for one session
-    counter = 0;
-    lastUpdatedTime = millis();
-    sentData = true;
-  }
-  oldStateTape = currentStateTape;
+  Serial.println(analogRead(sensorPin));
 }
--- photonCode/photonCode.c
+++ photonCode/photonCode.c
@@ -1,73 +1,25 @@
-// This #include statement was automatically added by the Particle IDE.
-
-/*
-//Commented out because the request library functionality was replaced with webhooks. Keep this around
-//for future reference in case webhooks decide to stop working.
-#include "HttpClient/HttpClient.h"
-
-HttpClient http;
-
-http_header_t headers[] = {
-{"Accept" , "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*\/*;q=0.8"},
-{"Accept-Encoding" , "gzip, deflate, sdch"},
-{"Accept-Language","en-US,en;q=0.8"},
-//Specifying content type in the header is important, otherwise the server application will not know how to parse the request body.
-{"Content-Type","application/x-www-form-urlencoded"},
-{"Connection","keep-alive"},
-//Host must be set to domain we are sending requests to otherwise requests are refused
-{"Host","bikechill.herokuapp.com"},
-{"Upgrade-Insecure-Requests","1"},
-{"User-Agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36"}
-};
-
-http_request_t request;
-http_response_t response;
-
-request.hostname = "bikechill.herokuapp.com";
-request.port = 80;
-request.path = "/bikeSession";
-*/
-
-const int reedSwitchPin = D0;
+const int reedSwitchPin = D2;
 
 long lastUpdatedTime = millis();
-long lastPosEdgeTime = 0;
-long DEBOUNCE_TIME = 5; //debounce time of 5 ms. Multiple positive edges happening within debounce time of each other are counted as one positive edge.
-long INACTIVE_THRESHOLD_TIME = 5000;
-long SLEEP_THRESHOLD_TIME = 20000;
-boolean lastSwitchVal;
-boolean broadcasted = true;
+long DEBOUNCE_TIME = 10;
+long SESSION_END_TIME = 10000; //After 10000 ms of idle time, session is considered to have ended.
 
 //number of measured wheel rotations in a session. reset to zero when no new rotations are detected for a while.
 int numberRotations = 0;
-
-long tempTime = millis();
+bool rotationsPosted = true;
 
 void setup() {
     lastUpdatedTime = millis();
+    attachInterrupt(reedSwitchPin,onReedSwitchHigh,RISING);
 }
 
-void detectPositiveEdge(boolean switchVal){
-  if(lastSwitchVal != switchVal && switchVal == HIGH){
-    if(millis() - lastPosEdgeTime > DEBOUNCE_TIME){
-      onPositiveEdgeDetected();
+void onReedSwitchHigh(){
+    long now = millis();
+    if(now - lastUpdatedTime > DEBOUNCE_TIME){
+        numberRotations++;
+        lastUpdatedTime = now;
+        rotationsPosted = false;
     }
-  }
-  lastSwitchVal = switchVal;
-}
-
-void onPositiveEdgeDetected(){
-  numberRotations++;
-  lastPosEdgeTime = millis();
-  lastUpdatedTime = millis();
-  broadcasted = false;
-}
-
-void printRPM(){
-   //uses last time positive edge was detected compared with current time to calculate rotations per second
-   long timeBetweenEdges = millis() - lastPosEdgeTime;
-   float RPM = 1.0/timeBetweenEdges * 60 * 1000;
-   Serial.println(RPM);  
 }
 
 void postRotations(){
@@ -76,30 +28,17 @@ void postRotations(){
     //to break easily and won't work when the device goes to sleep and
     //wakes up again
     Particle.publish("postRotations", String(numberRotations), PRIVATE);
+    rotationsPosted = true;
 }
 
-void broadcastNumberRotations(){
-  if (((millis() - lastUpdatedTime) > INACTIVE_THRESHOLD_TIME) && !broadcasted){
-    postRotations();
+void resetRotations(){
     numberRotations = 0;
-    lastUpdatedTime = millis();
-    broadcasted = true; //this way we won't broadcast data until we see another positive edge
-  }
-}
-
-void sleepIfIdle(){
-    if(broadcasted){
-        if((millis() - lastUpdatedTime) > SLEEP_THRESHOLD_TIME){
-            System.sleep(reedSwitchPin, CHANGE);
-        }
-    }
 }
 
 void loop() {
-  boolean IRVal = digitalRead(reedSwitchPin);
-  detectPositiveEdge(IRVal);
-  if (WiFi.ready()){
-    broadcastNumberRotations();
-    sleepIfIdle();
-  }
+    bool sessionEnded = (millis() - lastUpdatedTime) > SESSION_END_TIME;
+    if(Particle.connected() && sessionEnded && !rotationsPosted){
+        postRotations();
+        resetRotations();
+    }
 }
